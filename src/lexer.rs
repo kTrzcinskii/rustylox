48c882c29c603803ff3700f53bca9587af7ad21e
kTrzcinskii <kt.trzcinski@gmail.com>
2024-08-03 18:03:27 +0200

    Finish lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -63,8 +63,8 @@ pub struct Lexer<'a> {
     line: usize,
     /// Source code characters
     source: Peekable<Chars<'a>>,
-    /// Source code length
-    length: usize,
+    /// Original source code string
+    original_source: &'a str,
 }
 
 /// Represents error occured while scanning with provided error message and line on which it occured
@@ -78,11 +78,13 @@ impl<'a> Lexer<'a> {
             current: 0,
             line: 1,
             source: source.chars().peekable(),
-            length: source.len(),
+            original_source: source,
         }
     }
 
     pub fn scan_token(&mut self) -> Result<Token, LexerError> {
+        self.skip_whitespaces();
+
         self.start = self.current;
 
         if self.is_at_end() {
@@ -90,6 +92,15 @@ impl<'a> Lexer<'a> {
         }
 
         let c = self.advance();
+
+        if c.is_alphabetic() {
+            return Ok(self.make_identifier_token(c));
+        }
+
+        if c.is_ascii_digit() {
+            return self.make_number_token();
+        }
+
         match c {
             '(' => return Ok(self.make_token(TokenType::LeftParen)),
             ')' => return Ok(self.make_token(TokenType::RightParen)),
@@ -101,7 +112,13 @@ impl<'a> Lexer<'a> {
             '-' => return Ok(self.make_token(TokenType::Minus)),
             '+' => return Ok(self.make_token(TokenType::Plus)),
             '*' => return Ok(self.make_token(TokenType::Star)),
-            '/' => return Ok(self.make_token(TokenType::Slash)),
+            '/' => {
+                if self.peek() == '/' {
+                    self.skip_comment();
+                    return self.scan_token();
+                }
+                return Ok(self.make_token(TokenType::Slash));
+            }
             '!' => {
                 return Ok(self.handle_one_or_two_character_token(
                     '=',
@@ -130,14 +147,15 @@ impl<'a> Lexer<'a> {
                     TokenType::Greater,
                 ))
             }
+            '"' => return self.make_string_token(),
             _ => {}
         }
 
-        Err(LexerError(String::from("Unexpected character."), self.line))
+        Err(self.create_error("Unexpected character."))
     }
 
     fn is_at_end(&self) -> bool {
-        self.current >= self.length
+        self.current >= self.original_source.len()
     }
 
     fn advance(&mut self) -> char {
@@ -152,13 +170,23 @@ impl<'a> Lexer<'a> {
             return false;
         }
 
-        // This should never panic as we check 'is_at_end' before this action
-        if expected != *self.source.peek().unwrap() {
+        if expected != self.peek() {
             return false;
         }
 
         self.advance();
         true
+    }
+
+    fn peek(&mut self) -> char {
+        match self.source.peek() {
+            Some(c) => *c,
+            None => '\0',
+        }
+    }
+
+    fn create_error(&self, message: &str) -> LexerError {
+        LexerError(message.into(), self.line)
     }
 
     fn make_token(&self, token_type: TokenType) -> Token {
@@ -180,5 +208,120 @@ impl<'a> Lexer<'a> {
             return self.make_token(two_chararter_case);
         }
         self.make_token(one_character_case)
+    }
+
+    fn skip_whitespaces(&mut self) {
+        let mut c = self.peek();
+        while c.is_whitespace() {
+            if c == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+            c = self.peek();
+        }
+    }
+
+    fn skip_comment(&mut self) {
+        // comment is until end of line or end of file
+        while self.peek() != '\n' && !self.is_at_end() {
+            self.advance();
+        }
+    }
+
+    fn make_string_token(&mut self) -> Result<Token, LexerError> {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return Err(self.create_error("Unterminated string"));
+        }
+
+        // consume closing '"'
+        self.advance();
+        Ok(self.make_token(TokenType::String))
+    }
+
+    fn make_number_token(&mut self) -> Result<Token, LexerError> {
+        while self.peek().is_ascii_digit() {
+            self.advance();
+        }
+
+        // check for fractional part
+        if self.match_current('.') {
+            if !self.peek().is_ascii_digit() {
+                return Err(self.create_error("Expected fractional part after '.'."));
+            }
+            while self.peek().is_ascii_digit() {
+                self.advance();
+            }
+        }
+
+        Ok(self.make_token(TokenType::Number))
+    }
+
+    fn make_identifier_token(&mut self, first_letter: char) -> Token {
+        let mut second_letter: Option<char> = Option::None;
+        while self.peek().is_alphanumeric() {
+            let c = self.advance();
+            if second_letter.is_none() {
+                second_letter = Option::Some(c);
+            }
+        }
+        self.make_token(self.figure_identifier_type(first_letter, second_letter))
+    }
+
+    // This approach with explicitly getting first and second letter is pretty hacky
+    // I did only because it's the only place where we need to access chars by index
+    // and because it only requires first two letters
+    // If one of these were false I would look for something better, but
+    // I think in this context it's fine
+    fn figure_identifier_type(&self, first_letter: char, second_letter: Option<char>) -> TokenType {
+        // We don't have any keyword that is only 1 letter long
+        if second_letter.is_none() {
+            return TokenType::Identifier;
+        }
+
+        match first_letter {
+            'a' => self.check_for_keyword(1, "nd", TokenType::And),
+            'c' => self.check_for_keyword(1, "lass", TokenType::Class),
+            'e' => self.check_for_keyword(1, "lse", TokenType::Else),
+            'f' => match second_letter {
+                Some('a') => self.check_for_keyword(2, "lse", TokenType::False),
+                Some('o') => self.check_for_keyword(2, "r", TokenType::For),
+                Some('u') => self.check_for_keyword(2, "n", TokenType::Fun),
+                Some(_) => TokenType::Identifier,
+                None => TokenType::Identifier,
+            },
+            'i' => self.check_for_keyword(1, "f", TokenType::If),
+            'n' => self.check_for_keyword(1, "il", TokenType::Nil),
+            'o' => self.check_for_keyword(1, "r", TokenType::Or),
+            'p' => self.check_for_keyword(1, "rint", TokenType::Print),
+            'r' => self.check_for_keyword(1, "eturn", TokenType::Return),
+            's' => self.check_for_keyword(1, "uper", TokenType::Super),
+            't' => match second_letter {
+                Some('h') => self.check_for_keyword(2, "is", TokenType::This),
+                Some('r') => self.check_for_keyword(2, "ue", TokenType::True),
+                Some(_) => TokenType::Identifier,
+                None => TokenType::Identifier,
+            },
+            'v' => self.check_for_keyword(1, "ar", TokenType::Var),
+            'w' => self.check_for_keyword(1, "hile", TokenType::While),
+            _ => TokenType::Identifier,
+        }
+    }
+
+    fn check_for_keyword(&self, skip: usize, expected: &str, token_type: TokenType) -> TokenType {
+        let start_index = self.start + skip;
+        let end_index = start_index + expected.len();
+
+        let actual = &self.original_source[start_index..end_index];
+        if actual == expected {
+            return token_type;
+        }
+        TokenType::Identifier
     }
 }
